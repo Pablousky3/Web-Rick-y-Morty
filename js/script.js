7567eb7a1a2c29f3f36aa70357b8e6600373da7f
Ch3dd4rGames <mauchox@gmail.com>
2025-05-21 01:51:09 +0200

    fix/nombres css y js

---
+++ js/script.js
@@ -0,0 +1,111 @@
+<<<<<<< Updated upstream:js/Scripts.js
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("info-form");
+  const alertBox = document.getElementById("form-alert");
+=======
+$(document).ready(function(){
+  const planets = [
+    $("#PinCanada"), 
+    $("#PinEstadosUnidos"), 
+    $("#PinBrasil"), 
+    $("#PinJapon"),
+    $("#PinAustralia"), 
+    $("#PinChina"), 
+    $("#PinEspana"), 
+    $("#PinInglaterra"), 
+    $("#PinArgentina")
+  ];
+  
+    fetch("https://rickandmortyapi.com/api/location/1,3,4,8,9,10,13,16,20")
+    .then(function(response)
+    {
+        return response.json();
+    })
+    .then(function(result)
+    {
+        console.log(result);
+        let planets = result.data;
+
+        planets.forEach(function(planet) 
+        {
+            let link = document.createElement('a');
+            link.href = 'game.html?id=' + planet.id;
+            gamesListCont.append(link);
+        });
+    })
+    .catch(function(err)
+    {
+        console.log(err);
+    })
+});
+>>>>>>> Stashed changes:js/script.js
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    alertBox.classList.remove("d-none");
+    form.reset();
+    setTimeout(() => {
+      alertBox.classList.add("d-none");
+    }, 5000);
+  });
+
+  // Interacción con pines
+  $(".pin").mouseenter(function () {
+    let country = $(this).data("country");
+    $(".Pin" + country + ".fa-solid.fa-location-pin.fa-beat-fade").css("animation-iteration-count", "infinite");
+    $(".Foto" + country + " img").css("opacity", "100%");
+    $(".Texto" + country).css("opacity", "100%");
+    $(".caja-foto" + country).css("opacity", "100");
+  });
+
+  $(".pin").mouseleave(function () {
+    let country = $(this).data("country");
+    $(".Pin" + country + ".fa-solid.fa-location-pin.fa-beat-fade").css("animation-iteration-count", "0");
+    $(".Foto" + country + " img").css("opacity", "0%");
+    $(".Texto" + country).css("opacity", "0%");
+  });
+
+  // Scroll suave
+  document.querySelectorAll('a.nav-link[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href');
+      const target = document.querySelector(targetId);
+
+      if (target) {
+        const offset = 70; // altura de la navbar
+        const targetPosition = target.getBoundingClientRect().top + window.pageYOffset - offset;
+
+        window.scrollTo({
+          top: targetPosition,
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
+
+
+  // Scrollspy personalizado
+  const sections = document.querySelectorAll("section[id]");
+  const navLinks = document.querySelectorAll(".nav-link");
+
+  window.addEventListener("scroll", () => {
+    let current = "";
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(section => {
+      const sectionTop = section.offsetTop - 80;
+      const sectionHeight = section.offsetHeight;
+      if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+        current = section.getAttribute("id");
+      }
+    });
+
+    navLinks.forEach(link => {
+      link.classList.remove("active");
+      if (link.getAttribute("href") === `#${current}`) {
+        link.classList.add("active");
+      }
+    });
+  });
+});
