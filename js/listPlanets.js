a4e407371b2e94e33e6a2aea1f54a03e0e81745e
Pablousky3 <pablus000@gmail.com>
2025-05-21 15:03:05 +0200

    Agregar transicion con portales, añadir efectos de estela detras del mouse y arreglar fallos con estos.

--- js/listPlanets.js
+++ js/listPlanets.js
@@ -1,37 +1,55 @@
-$(document).ready(function(){
+$(document).ready(function () {
   const pins = [
-    $(".PinCanada"), 
-    $(".PinEstadosUnidos"), 
-    $(".PinBrasil"), 
+    $(".PinCanada"),
+    $(".PinEstadosUnidos"),
+    $(".PinBrasil"),
     $(".PinJapon"),
-    $(".PinAustralia"), 
-    $(".PinChina"), 
-    $(".PinEspana"), 
-    $(".PinInglaterra"), 
+    $(".PinAustralia"),
+    $(".PinChina"),
+    $(".PinEspana"),
+    $(".PinInglaterra"),
     $(".PinArgentina")
   ];
-  
-    fetch("https://rickandmortyapi.com/api/location/1,3,4,8,9,10,13,16,20")
-    .then(function(response)
-    {
-        return response.json();
-    })
-    .then(function(result)
-    {
-        console.log(result);
-        let planets = result;
-        let counter = 0;
 
-        planets.forEach(function(planet) 
-        {
-          let link = document.createElement('a');
-          link.href = 'location.html?id=' + planet.id;
-          pins[counter].append(link);
-          ++counter;
+  fetch("https://rickandmortyapi.com/api/location/1,3,4,8,9,10,13,16,20")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (result) {
+      let planets = result;
+      let counter = 0;
+      planets.forEach(function (planet) {
+        pins[counter].on("click", function () {
+          const loader = document.getElementById("portal-loader");
+          if (loader) {
+            loader.classList.add("portal-open");
+            sessionStorage.setItem("portalTransition", "true");
+            requestAnimationFrame(() => {
+              setTimeout(() => {
+                window.location.href = `location.html?id=${planet.id}`;
+              }, 1400); // tiempo extendido para suavidad
+            });
+          } else {
+            window.location.href = `location.html?id=${planet.id}`;
+          }
         });
+        ++counter;
+      });
     })
-    .catch(function(err)
-    {
-        console.log(err);
-    })
+    .catch(function (err) {
+      console.log(err);
+    });
+
+  // Efecto de partículas del cursor
+  document.addEventListener("mousemove", function (e) {
+    const particle = document.createElement("div");
+    particle.className = "cursor-particle";
+    particle.style.left = `${e.clientX}px`;
+    particle.style.top = `${e.clientY}px`;
+    document.body.appendChild(particle);
+    setTimeout(() => {
+      particle.remove();
+    }, 1000);
+  });
 });
+
